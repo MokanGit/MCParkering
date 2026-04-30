7e6b70daeff5f722b28cc794b5c34caab9d24f2d
MokanMartin <martin.koch@mokan.tech>
2026-04-30 10:47:53 +0200

    Initial Commit

---
+++ MCParkering/MCParkeringApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCParkeringApp.swift
+//  MCParkering
+//
+//  Created by Martin Koch on 2026-04-30.
+//
+
+import SwiftUI
+
+@main
+struct MCParkeringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
